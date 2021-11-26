c180f60392b1e070d21300a95da7b1feb407eafe
albertoconcovia <concovia@gmail.com>
2021-11-26 11:39:49 +0000

    Activity 3 - Activity 3 - Character pagination completed - useEffect updated into App.js

--- src/App.js
+++ src/App.js
@@ -3,22 +3,23 @@ import React, { useState, useEffect } from "react";
 import Header from "./components/Header";
 import CharacterContainer from "./components/CharacterContainer";
 import Navigation from "./components/Navigation";
-import axios from 'axios';
+import axios from "axios";
 
 function App() {
-
   const [characters, setCharacters] = useState([]);
 
   const [currentPage, setCurrentPage] = useState(1);
 
   const getCharacters = async (pageNumber) => {
-    const apiResponse = await axios.get(`http://api.disneyapi.dev/characters?page=${pageNumber}`);
+    const apiResponse = await axios.get(
+      `http://api.disneyapi.dev/characters?page=${pageNumber}`
+    );
     setCharacters(apiResponse.data.data);
   };
 
   useEffect(() => {
-    getCharacters(1);
-  }, []);
+    getCharacters(currentPage);
+  }, [currentPage]);
   
 
   return (
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,9 +1,7 @@
 function Character({ character }) {
 
-  // Define a default in case the character doesn't have an image
   let imageSrc = "https://picsum.photos/300/200/?blur";
   if (character.imageUrl) {
-    // API seems to include extra path for images so here we strip it off to fetch raw image
     imageSrc = character.imageUrl.substring(0, character.imageUrl.indexOf('/revision'));
   }
 
